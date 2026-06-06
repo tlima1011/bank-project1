97c61d14043c1ebdc2a93cb6823051e1d7f9cf85
tlima1011 <thiagolima1011@gmail.com>
2026-06-06 15:00:02 -0300

    Classe DomainException Created

---
+++ src/com/bank/project/exceptions/DomainException.java
@@ -0,0 +1,11 @@
+package com.bank.project.exceptions;
+
+public class DomainException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+	
+	public DomainException(String message) {
+		super(message); 
+	}
+
+}
