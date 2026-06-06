404d1a68b7d9a2ce7aee3d4fe28daec9801a038d
tlima1011 <thiagolima1011@gmail.com>
2026-06-06 14:42:40 -0300

    Projeto Criado

---
+++ src/com/bank/project/app/Main.java
@@ -0,0 +1,11 @@
+package com.bank.project.app;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Novo Projeto criado - Java - Reservation");
+
+	}
+
+}
---
+++ src/com/bank/project/entities/Account.java
@@ -0,0 +1,7 @@
+package com.bank.project.entities;
+
+public class Account {
+	
+	 
+
+}
