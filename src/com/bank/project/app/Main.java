9b8b67b093bf64201c63ba917d84895538519422
tlima1011 <thiagolima1011@gmail.com>
2026-06-06 16:26:44 -0300

    General Exception and Test Final

--- src/com/bank/project/app/Main.java
+++ src/com/bank/project/app/Main.java
@@ -8,7 +8,7 @@ import com.bank.project.exceptions.DomainException;
 
 public class Main {
 
-	public static void main(String[] args) throws DomainException {
+	public static void main(String[] args) throws DomainException, Exception {
 		
 		Locale.setDefault(Locale.US); 
 		Scanner ler = new Scanner(System.in); 
@@ -16,29 +16,27 @@ public class Main {
 		try {
 			System.out.println("Enter account data" );
 			System.out.print("Number: ");
-			//8021
 			int number = ler.nextInt(); 
 			ler.nextLine();
 			System.out.print("Holder: ");
 			String holder = ler.nextLine();  
-			//Bob Brown
 			System.out.print("Initial balance: ");
 			double balance = ler.nextDouble();
-			//500.00
 			System.out.print("Withdraw limit: ");
 			double withdrawLimit = ler.nextDouble();
-			//300.00 
-			//Enter amount for withdraw: 100.00
+			
 			Account account = new Account(number, holder, balance, withdrawLimit);
+			
 			System.out.print("Enter amount for withdraw: ");
 			double amount = ler.nextDouble(); 
 			account.WithDraw(amount); 
 			System.out.print("New balance: " + account);
 			
-			// 400.00	
 			ler.close();
 		}catch(DomainException e) {
 			System.out.println("Withdraw error: "+ e.getMessage());
+		}catch(java.util.InputMismatchException e) {
+			System.out.println("Error: formato ilegal com string informada");
 		}
 	}
 }
