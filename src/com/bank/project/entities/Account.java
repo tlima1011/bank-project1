1e13d45d7942bd711a1220771dada4b3c6a10a64
tlima1011 <thiagolima1011@gmail.com>
2026-06-06 14:49:38 -0300

    Class Account Created

--- src/com/bank/project/entities/Account.java
+++ src/com/bank/project/entities/Account.java
@@ -1,7 +1,28 @@
 package com.bank.project.entities;
 
 public class Account {
-	
-	 
+
+	private int Number;
+	private String Holder;
+	private double Balance;
+	private double WithDrawLimit;
+
+	public Account() {
+
+	}
+
+	public Account(int number, String holder, double balance, double withDrawLimit) {
+		Number = number;
+		Holder = holder;
+		Balance = balance;
+		WithDrawLimit = withDrawLimit;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder s = new StringBuilder();
+		s.append(String.format("%.2f", Balance));
+		return s.toString();
+	}
 
 }
