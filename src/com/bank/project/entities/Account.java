2acfe00068e2e61add0507f0a9b1b5259e6204d2
tlima1011 <thiagolima1011@gmail.com>
2026-06-06 15:10:28 -0300

    Method Deposit Created in Account

--- src/com/bank/project/entities/Account.java
+++ src/com/bank/project/entities/Account.java
@@ -1,5 +1,7 @@
 package com.bank.project.entities;
 
+import com.bank.project.exceptions.DomainException;
+
 public class Account {
 
 	private int Number;
@@ -11,12 +13,24 @@ public class Account {
 
 	}
 
-	public Account(int number, String holder, double balance, double withDrawLimit) {
+	public Account(int number, String holder, double balance, double withDrawLimit) throws DomainException {
 		Number = number;
 		Holder = holder;
-		Balance = balance;
+		Deposit(balance);
+		//Balance = balance;
 		WithDrawLimit = withDrawLimit;
 	}
+	
+	public void Deposit(double amount) throws DomainException { 
+		if(amount < 0) {
+			throw new DomainException("Valor do depósito não pode ser menor que 0");
+		}
+		else {
+			Balance += amount; 
+		}
+	}
+	
+	
 
 	@Override
 	public String toString() {
